8238359583b9b41780a2ee0467745273a86280a1
FacundoLavagnino1993 <facu9396@gmail.com>
2016-03-16 10:31:15 -0300

    Saludo al mundo

--- LaboratorioDosEjercitacioUno/LaboratorioDosEjercitacioUno/Program.cs
+++ LaboratorioDosEjercitacioUno/LaboratorioDosEjercitacioUno/Program.cs
@@ -9,6 +9,8 @@ namespace LaboratorioDosEjercitacioUno
     {
         static void Main(string[] args)
         {
+            Console.WriteLine("Hola mundo");
+            Console.ReadLine();
         }
     }
 }
