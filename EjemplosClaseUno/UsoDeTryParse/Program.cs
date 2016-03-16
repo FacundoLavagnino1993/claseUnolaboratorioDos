d09f56a4ecb0daefdbede1f2c8955a3b2f8c2e48
FacundoLavagnino1993 <facu9396@gmail.com>
2016-03-16 11:24:20 -0300

    Con el try parse puedo validar con una iteracion while, esto evita generar una excepcion.

--- EjemplosClaseUno/UsoDeTryParse/Program.cs
+++ EjemplosClaseUno/UsoDeTryParse/Program.cs
@@ -11,11 +11,15 @@ namespace UsoDeTryParse
         {
             Console.Title = "Uso de try parse";
             string dato;
-            Console.WriteLine("Ingrese su edad");
+            Console.WriteLine("Ingrese su edad: ");
             dato = Console.ReadLine();
             int numero;
-            numero = int.Parse(dato);
-
+            //numero = int.Parse(dato);
+            while (int.TryParse(dato, out numero) == false)
+            {
+                Console.WriteLine("Error intente reingresando un numero");
+                dato = Console.ReadLine();
+            }
         }
     }
 }
